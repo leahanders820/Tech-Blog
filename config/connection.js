cb234155892a2cd975451f08b1dbdb32f56c4c90
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-08-14 18:50:53 -0500

    asdsa

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+
+
 // Set up the database connection
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'process.env.DB_HOST', // Change this to your database host
   dialect: 'mysql', // Change this if you're using a different database
   port: 3306, // Change this to your database port
